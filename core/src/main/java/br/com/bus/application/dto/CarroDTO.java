266eef990a77b842db8682ba70dedbde86669d23
Arthur Sampaio <arthurbarrasampaio@gmail.com>
2025-10-01 15:23:46 -0300

    Update CarroDTO.java
    teste git

--- core/src/main/java/br/com/bus/application/dto/CarroDTO.java
+++ core/src/main/java/br/com/bus/application/dto/CarroDTO.java
@@ -2,4 +2,14 @@ package br.com.bus.application.dto;
 
 public class CarroDTO {
 
+    private String categoria;
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
 }
